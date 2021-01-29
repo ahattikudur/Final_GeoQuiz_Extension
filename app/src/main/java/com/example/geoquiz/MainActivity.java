16596e2b5e35637d99f35d4f246c702d7e803dce
Adit Hattikudur <adithattikudur@Adits-MacBook-Air.local>
2021-01-29 09:18:22 -0800

    Initial commit

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.geoquiz;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast ctoast = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                ctoast.setGravity(Gravity.TOP, 0, 40);
+                ctoast.show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast itoast = Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                itoast.setGravity(Gravity.TOP, 0, 40);
+                itoast.show();
+            }
+        });
+    }
+}
